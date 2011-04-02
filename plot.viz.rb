72e45c0e7b23710c1f96d235419cfaf226f69c5e
Todd Fisher <todd.fisher@gmail.com>
2011-04-02 14:31:17 -0400

    output the noisy parts of the audio file

--- plot.viz.rb
+++ plot.viz.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'gruff'
+# given an input stream of wave lengths plot into a wave graph... NOTE: this is not optimized for large wave files
 
 g = Gruff::Line.new
 g.data "Wave", STDIN.read.split(' ').map {|i| i.to_i }
--- splitter.c
+++ splitter.c
@@ -1,44 +1,119 @@
+/*
+ * Copyright (C) 2011 Todd A Fisher <todd.fisher@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Given an input wav file locate quiet portions of audio stream and clip them, 
+ * outputing a new stream of audio chunks that include only the noisy parts of the audio file.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <sndfile.h>
 
-static int split_by_silence(const char *file);
-static sf_count_t locate_silence_offset(SNDFILE *sndfile, SF_INFO *sndfile_info, sf_count_t soff, sf_count_t *offset);
+typedef struct _silence_detector {
+	SNDFILE *sndfile;
+	SF_INFO sndfile_info;
+  const char *file_name;
+  int threshold;
+	size_t buffer_size; // read 32 frames at a time?
+	int *buffer;
+
+  int chunk_count;
+	SNDFILE *out_sndfile;
+} SilenceDetector;
+
+static SilenceDetector* silence_detector_new(const char *file, int threshold);
+static void silence_detector_free(SilenceDetector *ptr);
+static int silence_detector_split_audio(SilenceDetector *ptr);
+static sf_count_t silence_detector_locate_silence_offset(SilenceDetector *ptr, sf_count_t soff, sf_count_t *offset);
+static void silence_detector_create_audio_chunk(SilenceDetector *ptr);
+static char* silence_detector_create_chunk_file_name(SilenceDetector *ptr);
+//static silence_detector_write_audio_chunk(SilenceDetector *ptr, sf_count_t 
 
 int main(int argc, char **argv) {
+  int threshold = 30;
 
 	if (argc < 2) {
 		fprintf(stderr, "usage: %s input_file\n", argv[0]);
 		return 1;
 	}
-	return split_by_silence(argv[1]);
+
+  if (argc > 2) {
+    threshold = atoi(argv[2]);
+    if (threshold < 0 ) {
+      fprintf(stderr, "threshold value %d should be greater than zero!\n", threshold);
+      return 2;
+    }
+  }
+  printf("using threshold: %d\n", threshold);
+
+  SilenceDetector *detector = silence_detector_new(argv[1], threshold);
+
+  silence_detector_split_audio(detector);
+
+  silence_detector_free(detector);
+
+  return 0;
 }
 
+static SilenceDetector* silence_detector_new(const char *file, int threshold) {
+  SilenceDetector *ptr = (SilenceDetector*)malloc(sizeof(SilenceDetector));
+  memset(ptr, 0, sizeof(SilenceDetector));
+  ptr->file_name = file;
 
+	ptr->sndfile = sf_open(file, SFM_READ, &(ptr->sndfile_info));
+  ptr->threshold = threshold;
 
-static int split_by_silence(const char *file) {
-	SNDFILE *sndfile = NULL;
-	SF_INFO sndfile_info;
+	if (!ptr->sndfile) {
+		fprintf(stderr, "Failed to open: %s with error: %s\n", file, sf_strerror(NULL));
+    free(ptr);
+		return NULL;
+	}
+	ptr->buffer_size = 1024; // read 32 frames at a time?
+	ptr->buffer = (int*)malloc(ptr->buffer_size * sizeof(int));
+
+  return ptr;
+}
+
+static void silence_detector_free(SilenceDetector *ptr) {
+  if (ptr) {
+    sf_close(ptr->sndfile);
+    free(ptr->buffer);
+
+    if (ptr->out_sndfile) {
+      sf_close(ptr->out_sndfile);
+    }
+
+    free(ptr);
+  }
+}
+
+static int silence_detector_split_audio(SilenceDetector *ptr) {
   sf_count_t offset = 0;
   sf_count_t prev_offset = 0;
   sf_count_t read_frames = 0;
   sf_count_t read_total = 0;
 
-	memset(&sndfile_info, 0, sizeof(SF_INFO));
-	sndfile = sf_open(file, SFM_READ, &sndfile_info);
-
-	if (!sndfile) {
-		fprintf(stderr, "Failed to open: %s with error: %s\n", file, sf_strerror(NULL));
-		return 1;
-	}
-
-  //printf("frame rate: %d\n", sndfile_info.samplerate);
+  silence_detector_create_audio_chunk(ptr);
 
   do {
     prev_offset = offset;
-    read_frames = locate_silence_offset(sndfile, &sndfile_info, offset, &offset);
+    read_frames = silence_detector_locate_silence_offset(ptr, offset, &offset);
     read_total += read_frames;
     //printf("p: %llu, o %llu\n", prev_offset, offset);
     if (prev_offset == offset) {
@@ -48,39 +123,65 @@ static int split_by_silence(const char *file) {
 
 	printf("\n");
 
-	sf_close(sndfile);
-
-	return 0;
+  return 0;
 }
 
-static sf_count_t locate_silence_offset(SNDFILE *sndfile, SF_INFO *sndfile_info, sf_count_t soff, sf_count_t *offset) {
-	int i, wave_length, avg = 0;
-	size_t buffer_size = 1024; // read 32 frames at a time?
-	int *buffer = (int*)malloc(buffer_size * sizeof(int));
-	sf_count_t read_bytes;
+static void silence_detector_create_audio_chunk(SilenceDetector *ptr) {
+  char *out_chunk_file_name = silence_detector_create_chunk_file_name(ptr);
+	ptr->out_sndfile = sf_open(out_chunk_file_name, SFM_WRITE, &(ptr->sndfile_info));
+  free(out_chunk_file_name);
 
-	read_bytes = sf_readf_int(sndfile, buffer, buffer_size);
-	for ( i = 0; i < read_bytes; ++i ) {
-    wave_length = buffer[i] / sndfile_info->samplerate;
+  if (!ptr->out_sndfile) {
+    fprintf(stderr, "Failed to create audio chunk!\n");
+    exit(3);
+  }
+}
+
+static char* silence_detector_create_chunk_file_name(SilenceDetector *ptr) {
+  size_t file_name_length = (strlen(ptr->file_name)+1 + strlen("chunk-") + 1 + ptr->chunk_count + 1);
+  char *out_chunk_file_name = (char*)malloc(file_name_length* sizeof(char));
+  memset(out_chunk_file_name, 0, file_name_length);
+  snprintf(out_chunk_file_name, file_name_length, "%s.chunk%d", ptr->file_name, ptr->chunk_count++);
+  printf("create new chunk: %s\n", out_chunk_file_name);
+  return out_chunk_file_name;
+}
+
+static sf_count_t silence_detector_locate_silence_offset(SilenceDetector *ptr, sf_count_t soff, sf_count_t *offset) {
+	int i, wave_length, avg = 0;
+	sf_count_t read_frames;
+
+	read_frames = sf_readf_int(ptr->sndfile, ptr->buffer, ptr->buffer_size);
+
+	for ( i = 0; i < read_frames; ++i ) {
+    wave_length = ptr->buffer[i] / (ptr->sndfile_info.samplerate);
     avg += abs(wave_length);
 		//printf("%.2d ", wave_length);
 	}
-  //printf("r: %llu, avg: %d\n", read_bytes, avg);
+  //printf("r: %llu, avg: %d\n", read_frames, avg);
 
-  if (read_bytes > 0 && avg > 0) {
-    avg /= read_bytes;
-    printf("\twave: %d\n", (int)avg);
+  if (read_frames > 0 && avg > 0) {
+    avg /= read_frames;
+    printf("\t%d\n", (int)avg);
     fflush(stdout);
   }
 
-  if (avg < 70) {
-    //*offset += soff; // start of the sequence is silence
+  if (avg > ptr->threshold) {
+    printf("\tnoisy here: %llu until %llu\n", *offset, ((*offset) + read_frames));
+    *offset += read_frames; // end of the sequence is possibly silence
+    // open the sound file if necessary
+    if (!ptr->out_sndfile) {
+      silence_detector_create_audio_chunk(ptr);
+    }
+    // write these bytes to our file 
+    sf_writef_int(ptr->out_sndfile, ptr->buffer, read_frames);
   }
   else {
-    *offset += read_bytes; // end of the sequence is possibly silence
+    printf("starting silence at %llu\n", *offset);
+    if (ptr->out_sndfile) {
+      sf_close(ptr->out_sndfile);
+      ptr->out_sndfile = NULL;
+    }
   }
 
-	free(buffer);
-
-	return read_bytes;
+	return read_frames;
 }
